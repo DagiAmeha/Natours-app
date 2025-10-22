e657a9aaedcfbafcdfd904c946266325ae1f1083
Dagi Ameha <dagimameha@gmail.com>
2025-10-22 13:59:26 +0300

    fixing issue

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTIOIN! Shitting down...');
   console.log(err.name, err.message);
-  console.log(err);
   process.exit(1);
 });
 
-// dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD,
-// );
-mongoose
-  .connect(process.env.DATABASE)
-  .then(() => console.log('DB connection successful!'));
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
