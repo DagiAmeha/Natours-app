bd2fce7f6512e75fb62d30d0e9b8f84fc3216ecc
Dagi Ameha <dagimameha@gmail.com>
2025-10-22 13:03:53 +0300

    preparing the app for production

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTIOIN! Shitting down...');
@@ -7,14 +7,16 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
